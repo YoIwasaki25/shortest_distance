8ec48bc24cad022da6ddbf721f78ba5b46d71555
岩﨑遥 <[october_sky@ruri.waseda.jp]>
2020-08-13 00:29:03 +0900

    [距離と料金が出せるようになった]

--- main.cpp
+++ main.cpp
@@ -4,63 +4,113 @@
 #include<string.h>
 #include<string>
 #include<vector>
+#include<array>
 
 using namespace std;
 
 #define COLUMNS 4
-#define __STDC_WANT_LTB_EXT1__ 1
+#define CONSUMPTION_TAX 1.1
 
-struct IC{
-    string name; //インター名
-    string dfm;    //三郷からの距離[km]
-    string dfy;    //谷和原からの距離[km]
-};
+typedef struct IC{
+    string route;       //ルート
+    string nn_1;        //node name 1
+    string nn_2;        //node name 2
+    string distance;    //distance
+    string highway;     //高速名
+    string node_1;
+    string node_2;
+}st;
 
-void input_file(){
-    string index;                  
-    string ic_name;             
-    string dist_from_misato;    
-    string dist_from_yawara;
+st* search_ic(string input){                  
     int i = 0;
-    string input_str;
+    int num;
 
-    struct IC ic[40]; //読み込みファイルの　index+1 以上のサイズを持つこと
-    struct IC* p;
+    struct IC ic[256]; //読み込みファイルの　index+1 以上のサイズを持つこと
+    struct IC *p;
 
     p = ic;
 
     string line;
 
-    ifstream myFileStream("testfile.csv");
+    ifstream myFileStream("ic_list.csv");
     if(!myFileStream.is_open()){
         cout << "File failed to open" << endl;
     }
     while(getline(myFileStream, line)){
         stringstream ss(line);
-        getline(ss, index, ',');
-        getline(ss, ic[i].name, ',');
-        getline(ss, ic[i].dfm, ',');
-        getline(ss, ic[i].dfy, ',');
-        cout << ic[i].name << endl;
-    i++;
+        getline(ss, ic[i].route, ',');
+        getline(ss, ic[i].nn_1, ',');
+        getline(ss, ic[i].nn_2, ',');
+        getline(ss, ic[i].distance, ',');
+        getline(ss, ic[i].highway, ',');
+        getline(ss, ic[i].node_1, ',');
+        getline(ss, ic[i].node_2, ',');
+        
+        i++;
     }
-    cout << "インターチェンジを入力してください" << endl;
-    cin >> input_str;
 
     for (;;){
-        if(p->name == ""){
+        if(p->nn_1 == ""){
             cout << "インターチェンジが見つかりません" << endl;
             break;
-        }else if(p->name == input_str){
-            cout << "三郷からの距離:　" << p->dfm << endl;
-            cout << "谷田部からの距離:　" << p->dfy << endl;
+        }else if(p->nn_1 == input){
+            num = stoi(p->route);
+            //cout << p->nn_1 << "から" << p->nn_2 << "までの距離" << p->distance << "km" << endl;
+            //cout << "高速名:　" << p->highway << endl;
+            //cout << "node" << p->node_1 << " to" << " node" << p->node_2 << endl;
             break;        
         }
         p++;
     }
+    return p;
+}
+
+double cost(double arg){
+    return (150 + 24.6 * arg) * CONSUMPTION_TAX;  
 }
 
 int main(){
-    input_file();
+    string input_start, input_end;
+    double distance;
+    double cst;
+    st* p; st* q; st* tmp;
+
+    cout << "出発ICを入力してください" << endl;
+    cin >> input_start;
+    p = search_ic(input_start);
+
+    cout << "到着ICを入力してください" << endl;
+    cin >> input_end;
+    q = search_ic(input_end);
+
+    if (stoi(q->route) - stoi(p->route) < 0){
+        tmp = p;
+        p = q;
+        q = tmp;
+    
+        for (;;){
+            if (stod(p->route) == stod(q->route))
+                break;
+            distance += stod(p->distance);
+            p++;
+        }
+        cout << p->nn_1 << "から" << q->nn_1 << "までの距離" << endl;
+        
+    }else{
+        for (;;){
+            if (stod(p->route) == stod(q->route))
+                break;
+
+            distance += stod(p->distance);
+            p++;
+        }
+        cout << p->nn_1 << "から" << q->nn_1 << "までの距離" << endl;
+    }
+    
+    cout << distance << "km" << endl;
+
+    cst = cost(distance);    
+
+    cout << " 料金:　" << cst << "円" << endl;
     return 0;
 }
